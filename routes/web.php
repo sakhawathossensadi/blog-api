fefc5cb9ed7c9e189665fd2b42cdac47169f3f90
sakhawathossensadi <newtonss919@gmail.com>
2022-08-26 10:48:36 +0600

    add api and web file

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group(
+    [
+        'prefix' => 'blog',
+        'middleware' => ['api'],
+    ],
+
+    static function (): void {
+        Route::get('api', function () {
+            return "hello";
+        });
+    }
+);
